5297fb27b2fd3a2827e50558570b01d82cd8b909
Michael Brookhuis <michael.brookhuis@suse.com>
2020-11-26 15:03:40 +0100

    Cleanup

--- opt/sumahub/hub_scp_bootstrap_repos.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-for x in $(spacecmd -q -- system_list|awk -F " : " '{ print $1 }');do echo ssh -o "StrictHostKeyChecking=no" root@$x "rm -r /srv/www/htdocs/pub/repositories/*"; scp -r -o "StrictHostKeyChecking=no" /srv/www/htdocs/pub/repositories/* root@$x:/srv/www/htdocs/pub/repositories;done
